6f86d84d57a1ef2db611301710c11ee2bfa56c9d
runoneall <runoobsteve@hotmail.com>
2026-02-05 09:52:03 +0800

    useful connect message

--- server/Serve.go
+++ server/Serve.go
@@ -13,6 +13,7 @@ func Serve(addr string, auth string) {
 	defer listener.Close()
 
 	fmt.Println("TCP Serve at:", addr)
+	fmt.Printf("Use 'uzi connect %s %s' to connect this server\n", listener.Addr().String(), auth)
 
 	for {
 		conn, err := listener.Accept()
